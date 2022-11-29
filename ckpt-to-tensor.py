4892b46e86f5ea50140d4cc7122f732c3988d487
Cryonicus <113840931+Cryonicus@users.noreply.github.com>
2022-11-29 11:29:24 +0100

    Delete ckpt-to-tensor.py

--- ckpt-to-tensor.py
+++
@@ -1,8 +0,0 @@
-###Convert dropped CKPT file to Tensor
-import torch
-import sys
-from safetensors.torch import save_file
-
-print(sys.argv)
-weights = torch.load(sys.argv[1])["state_dict"]
-save_file(weights,f"{sys.argv[1][:-5]}.safetensors")
